f81f92307d1a0866864878efbb83001a61d5c4a9
unknown <1gamercomum@gmail.com>
2025-05-13 01:14:49 -0300

    Fetch solicitation by id

---
+++ src/controllers/solicitation/GetById.ts
@@ -0,0 +1,29 @@
+import { IController, IExpressRequest, IExpressResponse } from '@datatypes/Controllers';
+import { EColetaRole, ISolicitation } from '@datatypes/Database';
+import { getSolicitationById, hideAttributes } from '@utils/Database';
+
+async function main(request: IExpressRequest, response: IExpressResponse) {
+    const id = parseInt(request.params.id);
+
+    if (isNaN(id)) {
+        response.status(400).json({ message: `'${id}' is not a number` });
+        return;
+    }
+
+    const result: ISolicitation | null = await getSolicitationById(id);
+
+    if (!result) {
+        response.status(404).json({ message: 'Not found' });
+        return;
+    }
+
+    response.json({ data: hideAttributes(result, [ '_id' ]) });
+}
+
+export const controller: IController = {
+    main,
+    path: '/:id',
+    method: 'GET',
+    authenticationRequired: true,
+    requiredRole: [EColetaRole.employee, EColetaRole.admin]
+}
